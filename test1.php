4b4f04dbdce345bde273ca4a9519ceacdf1db6f1
kowabungag <56539235+kowabungag@users.noreply.github.com>
2020-07-11 12:58:10 +0300

    Add files via upload

--- test1.php
+++ test1.php
@@ -2,6 +2,10 @@
 abstract class auto
 {
     public $name;
+    public $seats;
+    public $color;
+    public $volume;
+    public $year;
     public $plate;
 
     public function __construct($name, $plate)
@@ -9,12 +13,13 @@ abstract class auto
      $this->name=$name;
      $this->plate=$plate;
    }
-
-
 }
 class truck extends auto
 {
+    public $time;
+    public $engine;
     public $capacity = 14000;
+    public $chassis;
 
     public function setcapacity($newcapacity)
     {
@@ -23,32 +28,44 @@ class truck extends auto
     }
     public function addcar($car)
     {
-      //  var_dump(array($car));
+      $result=$this->capacity-$car->weight;
+      $this->capacity = $result;
+      if($this->capacity > 0)
+      {
+      } else
+        {
+            echo "Машина превысила лимит веса на" . " " . $this->capacity . ' ';
+            echo $car->name . '-';
+                echo $car->plate . '</br>';
+        }
+        return $this;
     }
 }
 class car extends auto
 {
+    public $cartime;
+    public $carengine;
+    public $cartype;
+    public $carfuel;
     public $weight;
     public function setweight($newweigth)
     {
-
-        $this->weight=$newweigth;
-        //   $this->weight=$this->weight+$newweigth;
-    //  echo $this->weight;
+       $this->weight=$newweigth;
+       return $this;
     }
 }
 
 $track = new truck('bmw1','AA0123BC');
-$track ->setcapacity(8000);
+$track ->setcapacity(5000);
 
 
 $cars[] = (new car('bmw','AA0123BC'))->setweight(2300);
 $cars[] = (new car('audi','AI0212BC'))->setweight(1900);
-$cars[] = (new car('renault','AE4587'))->setweight(850);
+$cars[] = (new car('renault','AC2333BC'))->setweight(2100);
+
 
 
 foreach ($cars as $car){
-    var_dump($car);
     $track->addcar($car);
 }
 
